10270a758c7529141cd9a9e7249b1e9db1ffced7
James <35391305+jameswalls@users.noreply.github.com>
2025-08-05 11:34:56 -0600

    nvim: remap accept key for copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,25 @@
 return {
 	"github/copilot.vim",
-	ft = { "python", "sh", "Dockerfile" },
+	lazy = true,
+	ft = { "python", "sh", "Dockerfile", "lua" },
+	config = function()
+		vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "solarized",
+			-- group = ...,
+			callback = function()
+				vim.api.nvim_set_hl(0, "CopilotSuggestion", {
+					fg = "#555555",
+					ctermfg = 8,
+					force = true,
+				})
+			end,
+		})
+
+		vim.g.copilot_no_tab_map = true
+	end,
 }
