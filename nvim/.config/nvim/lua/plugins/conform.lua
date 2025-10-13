58c8efa9ce2be97e91ac6f9e5fd140de62c214e0
james walls <35391305+jameswalls@users.noreply.github.com>
2025-10-13 01:15:31 -0600

    nvim: conform rustfmt

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,12 @@ return {
 	"stevearc/conform.nvim",
 	config = function()
 		require("conform").setup({
+			formatters = {
+				rustfmt = {
+					command = "rustfmt",
+					args = { "--config-path", vim.fn.expand("~/.config/rustfmt/rustfmt.toml") },
+				},
+			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- python = { "black" },
