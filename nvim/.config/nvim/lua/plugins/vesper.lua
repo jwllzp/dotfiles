93c9c7d0c3355a7b6d3ae1c6671b0e47b3d15c28
James <35391305+jameswalls@users.noreply.github.com>
2025-11-07 18:06:46 -0600

    nvim: vesper spell

--- nvim/.config/nvim/lua/plugins/vesper.lua
+++ nvim/.config/nvim/lua/plugins/vesper.lua
@@ -33,6 +33,7 @@ return {
 				FloatBorder = { fg = colors.border },
 				NormalFloat = { bg = "none" },
 				Statement = { fg = mix(colors.primary, "#000000", 0.60), bold = true },
+				Operator = { fg = colors.symbol },
 				TelescopeBorder = { fg = colors.border },
 				TelescopeMatching = { fg = colors.red, bold = true },
 				TelescopeSelection = { bg = colors.fgSelection },
@@ -47,3 +48,10 @@ return {
 		vim.cmd.colorscheme("vesper")
 	end,
 }
+-- set_hl(0, 'RainbowDelimiterRed'   , {default = true, fg = '#cc241d', ctermfg= 'Red'    })
+-- set_hl(0, 'RainbowDelimiterOrange', {default = true, fg = '#d65d0e', ctermfg= 'White'  })
+-- set_hl(0, 'RainbowDelimiterYellow', {default = true, fg = '#d79921', ctermfg= 'Yellow' })
+-- set_hl(0, 'RainbowDelimiterGreen' , {default = true, fg = '#689d6a', ctermfg= 'Green'  })
+-- set_hl(0, 'RainbowDelimiterCyan'  , {default = true, fg = '#a89984', ctermfg= 'Cyan'   })
+-- set_hl(0, 'RainbowDelimiterBlue'  , {default = true, fg = '#458588', ctermfg= 'Blue'   })
+-- set_hl(0, 'RainbowDelimiterViolet', {default = true, fg = '#b16286', ctermfg= 'Magenta'})
