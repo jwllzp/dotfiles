f6d8a8c2c71d3325e8097c0ce103a1cbe0f47f17
James <35391305+jameswalls@users.noreply.github.com>
2025-11-07 18:59:21 -0600

    nvim: vesper variable builtint

--- nvim/.config/nvim/lua/plugins/vesper.lua
+++ nvim/.config/nvim/lua/plugins/vesper.lua
@@ -45,15 +45,9 @@ return {
 				RainbowDelimiter3 = { fg = "#458588" },
 				["@spell"] = { fg = colors.comment },
 				["@string.documentation.python"] = { fg = colors.comment },
+				["@variable.builtin"] = { fg = colors.red },
 			},
 		})
 		vim.cmd.colorscheme("vesper")
 	end,
 }
--- set_hl(0, 'RainbowDelimiterRed'   , {default = true, fg = '#cc241d', ctermfg= 'Red'    })
--- set_hl(0, 'RainbowDelimiterOrange', {default = true, fg = '#d65d0e', ctermfg= 'White'  })
--- set_hl(0, 'RainbowDelimiterYellow', {default = true, fg = '#d79921', ctermfg= 'Yellow' })
--- set_hl(0, 'RainbowDelimiterGreen' , {default = true, fg = '#689d6a', ctermfg= 'Green'  })
--- set_hl(0, 'RainbowDelimiterCyan'  , {default = true, fg = '#a89984', ctermfg= 'Cyan'   })
--- set_hl(0, 'RainbowDelimiterBlue'  , {default = true, fg = '#458588', ctermfg= 'Blue'   })
--- set_hl(0, 'RainbowDelimiterViolet', {default = true, fg = '#b16286', ctermfg= 'Magenta'})
