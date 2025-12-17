c6b0ba3d570249b7304d449e2082b00c80430d36
James <35391305+jameswalls@users.noreply.github.com>
2025-12-16 19:18:54 -0600

    nvim: update treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	branch = "main",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "scala", "json", "rust" },
-			modules = {},
-			ignore_install = {},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<leader><space>",
-					node_incremental = "<space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
+		require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "query", "python", "scala", "json", "rust" })
 	end,
 }
