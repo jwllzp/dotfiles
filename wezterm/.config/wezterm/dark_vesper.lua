3b16d7213cd51db24e27e7e3cac665cd0d8ea6c4
jwllzp <35391305+jwllzp@users.noreply.github.com>
2025-12-17 01:43:19 -0600

    hypr: add dark vesper for wezterm

---
+++ wezterm/.config/wezterm/dark_vesper.lua
@@ -0,0 +1,31 @@
+return {
+  foreground = "#ffffff",
+  background = "#000000",
+  cursor_bg = "#acb1ab",
+  cursor_fg = "#ffffff",
+  cursor_border = "#acb1ab",
+  selection_bg = "#988049",
+  selection_fg = "#b9beb8",
+
+  ansi = {
+    "#101010", -- black
+    "#f5a191", -- red
+    "#90b99f", -- green
+    "#e6b99d", -- yellow
+    "#aca1cf", -- blue
+    "#e29eca", -- magenta
+    "#ea83a5", -- cyan
+    "#a0a0a0", -- white
+  },
+
+  brights = {
+    "#7e7e7e", -- bright black
+    "#ff8080", -- bright red
+    "#99ffe4", -- bright green
+    "#ffc799", -- bright yellow
+    "#b9aeda", -- bright blue
+    "#ecaad6", -- bright magenta
+    "#f591b2", -- bright cyan
+    "#ffffff", -- bright white
+  },
+}
