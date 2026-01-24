c8b217ae8abf094df757e8cdbb3f9e32a18c9201
james walls <35391305+jameswalls@users.noreply.github.com>
2026-01-24 15:04:28 -0600

    nvim: koda rainbow highlights

--- nvim/.config/nvim/lua/plugins/koda.lua
+++ nvim/.config/nvim/lua/plugins/koda.lua
@@ -15,7 +15,11 @@ return {
 				constants = {}, -- includes numbers, booleans
 			},
 			colors = {},
-			on_highlights = function(hl, c) end,
+			on_highlights = function(hl, _)
+				hl.RainbowDelimiter1 = { fg = "#ffd700" }
+				hl.RainbowDelimiter2 = { fg = "#da70d6" }
+				hl.RainbowDelimiter3 = { fg = "#179fff" }
+			end,
 		})
 		vim.cmd.colorscheme("koda")
 	end,
