aab74837bc5b043de19793c578ac5438d473431f
james walls <35391305+jameswalls@users.noreply.github.com>
2026-01-24 14:48:26 -0600

    nvim: koda

--- nvim/.config/nvim/lua/plugins/koda.lua
+++ nvim/.config/nvim/lua/plugins/koda.lua
@@ -1,9 +1,22 @@
 return {
-  "oskarnurm/koda.nvim",
-  priority = 1000,
-  lazy = false,
-  config = function()
-    vim.cmd.colorscheme("koda")
-  end
+	"oskarnurm/koda.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require("koda").setup({
+			transparent = true,
+			auto = true,
+			cache = true,
+			styles = {
+				functions = { bold = true },
+				keywords = { bold = true },
+				comments = {},
+				strings = {},
+				constants = { bold = true }, -- includes numbers, booleans
+			},
+			colors = {},
+			on_highlights = function(hl, c) end,
+		})
+		vim.cmd.colorscheme("koda")
+	end,
 }
-
