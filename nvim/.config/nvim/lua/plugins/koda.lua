19ef6ee1ff2cc8a514b7bdba710eb4ebb28289b9
jwllzp <35391305+jwllzp@users.noreply.github.com>
2026-02-06 22:21:15 -0600

    nvim: koda settings

--- nvim/.config/nvim/lua/plugins/koda.lua
+++ nvim/.config/nvim/lua/plugins/koda.lua
@@ -2,13 +2,14 @@ return {
 	"oskarnurm/koda.nvim",
 	priority = 1000,
 	lazy = false,
+	dev = true,
 	config = function()
 		require("koda").setup({
-			transparent = true,
+			transparent = false,
 			auto = true,
-			cache = true,
+			cache = false,
 			styles = {
-				functions = {},
+				functions = { bold = false },
 				keywords = {},
 				comments = {},
 				strings = {},
@@ -16,9 +17,9 @@ return {
 			},
 			colors = {},
 			on_highlights = function(hl, _)
-				hl.RainbowDelimiter1 = { fg = "#ffd700" }
-				hl.RainbowDelimiter2 = { fg = "#da70d6" }
-				hl.RainbowDelimiter3 = { fg = "#179fff" }
+				-- hl.RainbowDelimiter1 = { fg = "#ffd700" }
+				-- hl.RainbowDelimiter2 = { fg = "#da70d6" }
+				-- hl.RainbowDelimiter3 = { fg = "#179fff" }
 			end,
 		})
 		vim.cmd.colorscheme("koda")
