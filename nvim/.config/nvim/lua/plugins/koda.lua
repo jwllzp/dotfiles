5d8bd3b29ac6ec9e2d9b300f19bdf03526ea30df
jwllzp <35391305+jwllzp@users.noreply.github.com>
2026-02-07 18:23:40 -0600

    nvim: restore non-development koda settings

--- nvim/.config/nvim/lua/plugins/koda.lua
+++ nvim/.config/nvim/lua/plugins/koda.lua
@@ -5,7 +5,7 @@ return {
 	dev = true,
 	config = function()
 		require("koda").setup({
-			transparent = false,
+			transparent = true,
 			auto = true,
 			cache = false,
 			styles = {
@@ -17,9 +17,9 @@ return {
 			},
 			colors = {},
 			on_highlights = function(hl, _)
-				-- hl.RainbowDelimiter1 = { fg = "#ffd700" }
-				-- hl.RainbowDelimiter2 = { fg = "#da70d6" }
-				-- hl.RainbowDelimiter3 = { fg = "#179fff" }
+				hl.RainbowDelimiter1 = { fg = "#ffd700" }
+				hl.RainbowDelimiter2 = { fg = "#da70d6" }
+				hl.RainbowDelimiter3 = { fg = "#179fff" }
 			end,
 		})
 		vim.cmd.colorscheme("koda")
