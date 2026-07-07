41adec73049b3fab4b82360180ab9444ff53dee8
jwls <35391305+jwllzp@users.noreply.github.com>
2026-07-07 13:15:10 -0600

    nvim: koda

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -14,5 +14,5 @@ require("plugins.telescope")
 require("plugins.treesitter")
 
 -- colorscheme must go at the end
-require("plugins.themes.catppuccin")
+require("plugins.themes.koda")
 require("highlights")
